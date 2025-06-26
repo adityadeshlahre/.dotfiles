ad6fa204977f7f83323b9adff0da2fde1d6df7b9
Aditya Deshlahre <wymaditya@gmail.com>
2025-06-26 22:16:06 +0530

    fix: tmux added

--- .fish/config.fish
+++ .fish/config.fish
@@ -1,6 +1,6 @@
-if status is-interactive
+# if status is-interactive
     # Commands to run in interactive sessions can go here
-end
+# end
 
 # Start X at login
 if status is-login
@@ -31,11 +31,6 @@ if type -q nvm
     nvm use latest >/dev/null
 end
 
-# GO
-set -Ux GOPATH $HOME/go
-set -Ux PATH $PATH $GOPATH/bin
-
-
 # rubyenv
 status --is-interactive; and source (rbenv init -|psub)
 
@@ -52,30 +47,37 @@ end
 # pnpm end
 
 # android-dev-start
-set -Ux ANDROID_HOME $HOME/Android/Sdk
-set -Ux ANDROID_SDK_ROOT $ANDROID_HOME
-if not string match -q -- $ANDROID_HOME/tools $PATH
-    set -gx PATH $ANDROID_HOME/tools $ANDROID_HOME/tools/bin $ANDROID_HOME/platform-tools $PATH
-end
+# set -Ux ANDROID_HOME $HOME/Android/Sdk
+# set -Ux ANDROID_SDK_ROOT $ANDROID_HOME
+# if not string match -q -- $ANDROID_HOME/tools $PATH
+    # set -gx PATH $ANDROID_HOME/tools $ANDROID_HOME/tools/bin $ANDROID_HOME/platform-tools $PATH
+# end
 # android-dev-stop
 
 # vim runtime
 set -Ux VIMRUNTIME /usr/share/nvim/runtime
 # vim runtime
 
+# GO
+set -Ux GOPATH $HOME/go
+# set -Ux PATH $PATH $GOPATH/bin
+
+# Add GOPATH bin to PATH
+set -U fish_user_paths $GOPATH/bin $fish_user_paths
+
 # gvm path
 
 function gvm
     bash -c "source ~/.gvm/scripts/gvm && gvm $argv"
 end
-bass source ~/.gvm/scripts/gvm
 
-# gvm path end
+# bass source ~/.gvm/scripts/gvm
+
 
 # set -Ux GVMPATH $HOME/.gvm/bin $GVMPATH
 # status --is-interactive; and source $HOME/.gvm/scripts/gvm
 
-# gvm path
+# gvm path end
 
 # Start ssh-agent if not already running
 # if not set -q SSH_AUTH_SOCK
--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 ##### Instruction 4 me
 
 put this 30-touchpad.conf to /etc/X11/xorg.conf.d/30-touchpad.conf 
+
+##### Fish Plugins
+
+fisher install JGAntunes/fish-gvm
+fisher install jethrokuan/z
+fisher install jorgebucaran/autopair.fish
+
+##### 
